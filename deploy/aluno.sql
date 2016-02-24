641c9f495b1e895f0c4e1174f372b7768bb0f97f
douglas <douglas.silva@armazemparaiba.com.br>
2016-02-24 11:49:13 -0300

    cria tabela aluno

---
+++ deploy/aluno.sql
@@ -0,0 +1,10 @@
+-- Deploy escola:aluno to pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+ CREATE TABLE escola.aluno (id bigserial primary key,
+        nome varchar(40) not null,cpf char(11),
+        dt_inc date not null default now());
+
+COMMIT;
